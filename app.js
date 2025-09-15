c89ace6c0a727de06ef07e2eafbe49355438940e
insan <insanbusted@gmail.com>
2025-09-15 20:25:54 +0700

    deploment

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import https from "https";
+import fs from "fs";
 import express from "express";
 import dotenv from "dotenv";
 import tiktokRoutes from "./routes/api.js";
@@ -7,17 +9,21 @@ import { swaggerUi, specs } from "./swagger.js";
 dotenv.config();
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api", tiktokRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-// Swagger docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
+// HTTPS
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+const sslOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/api.ratcs.my.id/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/api.ratcs.my.id/fullchain.pem")
+};
+
+https.createServer(sslOptions, app).listen(PORT, '0.0.0.0', () => {
+  console.log(`HTTPS Server running on port ${PORT}`);
 });
