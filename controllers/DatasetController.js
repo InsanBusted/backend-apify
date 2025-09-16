34692e74cf50102388a386965385088b6f8ac00d
insan <insanbusted@gmail.com>
2025-09-16 10:43:52 +0700

    fix create date

--- controllers/DatasetController.js
+++ controllers/DatasetController.js
@@ -165,7 +165,7 @@ class DatasetController {
           },
         },
         orderBy: {
-          createTime: "desc",
+          createDate: "desc",
         },
       });
 
--- models/tiktok.js
+++ models/tiktok.js
@@ -12,8 +12,8 @@ class Tiktok {
 
       const taskInput = {
         excludePinnedPosts: false,
-        newestPostDate: "2025-09-15",
-        oldestPostDateUnified: "2024-09-16",
+        newestPostDate: "2025-09-16",
+        oldestPostDateUnified: "2025-08-15",
         profileScrapeSections: ["videos"],
         profiles: Array.isArray(input) ? input : [input],
         resultsPerPage: 50,
