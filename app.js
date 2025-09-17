1a1078fb69aba7936943db2d94c3ecd359036482
insan <insanbusted@gmail.com>
2025-09-17 19:42:20 +0700

    update

--- app.js
+++ app.js
@@ -17,13 +17,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-// HTTPS
 const PORT = process.env.PORT || 5000;
-const sslOptions = {
-  key: fs.readFileSync("/etc/letsencrypt/live/api.ratcs.my.id/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/api.ratcs.my.id/fullchain.pem")
-};
-
-https.createServer(sslOptions, app).listen(PORT, '0.0.0.0', () => {
-  console.log(`HTTPS Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
